c15c286c72dec32fc5d603f08808c0b627d6c339
Syed Rashwan Uzaib <rashwanuzaib2002@gmail.com>
2025-01-26 19:35:11 +0400

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify   
 from web3 import Web3
 
 app = Flask(__name__)
@@ -32,6 +32,4 @@ def get_records():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
 
